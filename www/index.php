dc942bfe721316515ed9c4d7fad7410c5062a304
treyzania <treyzania@gmail.com>
2016-02-24 08:33:46 -0500

    Fixed "de1337ed".

--- www/index.php
+++ www/index.php
@@ -15,7 +15,7 @@
 			<div class="panel">
 				
 				<h1>I made an oopsie.</h1>
-		I accidentally deleeted everything in the root directory of my website.  I&quot;m working to fix this right now.<br/>
+		I accidentally deleted everything in the root directory of my website.  I&quot;m working to fix this right now.<br/>
 		But you can visit my <a href="http://github.com/treyzania">GitHub account</a> for some of the stuff that I had here, including this website because I'm remaking it anew!  And then there's also <a href="https://play.google.com/store/apps/details?id=com.treyzania.pew">Pew</a> that I had here as well.
 				
 			</div>
