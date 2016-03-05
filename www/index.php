4277227f129619b4ab5c8bff7b06456f6dca1a64
treyzania <treyzania@gmail.com>
2016-03-05 13:23:22 -0500

    Added portfolio link, to a page not currently in this repo.

--- www/index.php
+++ www/index.php
@@ -15,8 +15,11 @@
 			<div class="panel">
 				
 				<h1>I made an oopsie.</h1>
-		I accidentally deleted everything in the root directory of my website.  I&quot;m working to fix this right now.<br/>
-		But you can visit my <a href="http://github.com/treyzania">GitHub account</a> for some of the stuff that I had here, including this website because I'm remaking it anew!  And then there's also <a href="https://play.google.com/store/apps/details?id=com.treyzania.pew">Pew</a> that I had here as well.
+				I accidentally deleted everything in the root directory of my website.  I&quot;m working to fix this right now.<br/>
+				But you can visit my <a href="http://github.com/treyzania">GitHub account</a> for some of the stuff that I had here, including this website because I'm remaking it anew!  And then there's also <a href="https://play.google.com/store/apps/details?id=com.treyzania.pew">Pew</a> that I had here as well.
+				
+				<br/>
+				I also found an old copy of my portfolio, which is only slightly out of date, and <a href="/portfolio.html">uploaded it here</a>.
 				
 			</div>
 			
